89a1f76f1a5c3a3650c6f26742bf8626ab2f871b
Arthur Lima <artglima.dev@gmail.com>
2024-10-31 10:53:56 -0300

    feat: post method creation

--- src/main/java/com/ignitec/transaction_api/controllers/TransactionController.java
+++ src/main/java/com/ignitec/transaction_api/controllers/TransactionController.java
@@ -1,9 +1,28 @@
 package com.ignitec.transaction_api.controllers;
 
+import com.ignitec.transaction_api.domain.dtos.TransactionDTO;
+import com.ignitec.transaction_api.domain.entities.Transaction;
+import com.ignitec.transaction_api.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transaction")
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Transaction> save(@RequestBody TransactionDTO transactionDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.save(transactionDTO));
+    }
 }
